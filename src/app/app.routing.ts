c26504783a68ce2a63e54e192be2898852d0b06b
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-17 11:29:38 -0700

    Import slide puzzle to project

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RunnerComponent } from './runner/runner.component';
+import { SlidePuzzleComponent } from './slide-puzzle/slide-puzzle.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +12,10 @@ const appRoutes: Routes = [
   {
     path: 'runner',
     component: RunnerComponent
+  },
+  {
+    path: 'slide-puzzle',
+    component: SlidePuzzleComponent
   }
 ];
 
